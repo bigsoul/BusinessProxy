ce1248e61ffcb3b3dd984f4133e7fdefe9b4ba35
bigsoul <vvv93rus@gmail.com>
2020-09-01 05:14:57 +0300

    Универсальная конфигурация

--- src/config.ts
+++ src/config.ts
@@ -1,11 +1,29 @@
-const origin = window.location.origin;
-const homepage = "/business_proxy";
+const port = window.location.port;
+
+let homepage: string;
+let service: string;
+
+// три набора конфигурации, идентифицируются по номеру прота
+
+if (port === "3000") {
+  // для лакальной разработки
+  homepage = "";
+  service = "http://185.26.205.42:8086/do_demo";
+} else if (port === "8085") {
+  // для продакшен версии
+  homepage = "/business_proxy";
+  service = "http://185.26.205.42:8085/do";
+} else if (port === "8086" || port === "8087") {
+  // для сервера разработки и тестирования
+  homepage = "/business_proxy";
+  service = "http://185.26.205.42:" + port + "/do_demo";
+} else {
+  throw Error("Задана не верная конфигурация приложения.");
+}
 
 const config = {
-  root: origin + homepage,
-  origin: origin,
   homepage: homepage,
-  serviceUrl: "http://185.26.205.42:8086/do_demo/hs/BusinessProxy/",
+  serviceUrl: service + "/hs/BusinessProxy/",
   serviceLogin: "exchange",
   servicePassword: "exchange2016",
 };
